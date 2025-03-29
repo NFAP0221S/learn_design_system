0cdc3f05c0057e29fd17f403b23bbfd6bd7525e5
hslee <vawav11@gmail.com>
2025-03-29 13:51:47 +0900

    ts, build 설정 완료

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,23 +1,43 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
+import pkg from "./package.json" assert { type: "json" };
 
-esbuild.build({
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outdir: 'dist',
-  format: 'esm'
-})
+const dev = process.argv.includes("--dev");
+const minify = !dev;
 
-// commonjs es module
-esbuild.build({
-  entryPoints: ['src/index.js'],
+const watch = process.argv.includes("--watch");
+
+const external = Object.keys({
+  // ...pkg.devDependencies,
+  ...pkg.dependencies,
+  ...pkg.peerDependencies,
+});
+
+const baseConfig = {
+  // entryPoints: ["src/index.js"],
+  entryPoints: ["src/index.ts"],
   bundle: true,
-  minify: true,
+  minify,
   sourcemap: true,
-  outdir: 'dist',
-  format: 'cjs',
-  outExtension: {
-    ".js": ".cjs",
-  }
-})
+  outdir: "dist",
+  target: "es2019",
+  watch,
+  external,
+};
+
+// 병렬처리
+Promise.all([
+  esbuild.build({
+    ...baseConfig,
+    format: "esm",
+  }),
+  esbuild.build({
+    ...baseConfig,
+    format: "cjs",
+    outExtension: {
+      ".js": ".cjs",
+    },
+  }),
+]).catch(() => {
+  console.error("Build failed");
+  process.exit(1);
+});
